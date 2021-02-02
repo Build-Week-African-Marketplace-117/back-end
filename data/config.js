31eabb70e5a8c7637160b5edac4b8b47f778a45f
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-02-01 21:08:36 -0600

    Added knexfile data from Joo

--- data/config.js
+++ data/config.js
@@ -1,4 +1,19 @@
-const knex = require("knex")
-const knexfile = require("../knexfile")
+const knex = require("knex");
+require("dotenv").config();
+const env = process.env.DB_ENV || "development";
+const config = require("../knexfile");
+const db = knex(config[env]);
+module.exports = db;
 
-module.exports = knex(knexfile.development)
+
+
+
+
+
+
+
+
+//const knex = require("knex")
+// const knexfile = require("../knexfile")
+
+// module.exports = knex(knexfile.development)
