1d8fc53364e989facaa3cf22a65e4f9e0db8398c
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-02-02 19:09:15 -0600

    resetting the index

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const usersRouter = require("./routers/users-router")
-const categoriesRouter = require("./routers/categories-router")
-const itemsRouter = require("./routers/items-router")
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const usersRouter = require("./routers/users-router");
+const categoriesRouter = require("./routers/categories-router");
+const itemsRouter = require("./routers/items-router");
 
-const server = express()
-const port = process.env.PORT || 5000
+const server = express();
+const PORT = process.env.PORT || 5000;
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
-server.use(cookieParser())
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(cookieParser());
 
-server.use("/api/users", usersRouter)
-server.use("/api/categories", categoriesRouter)
-server.use("/api/items", itemsRouter)
+server.use("/api/users", usersRouter);
+server.use("/api/categories", categoriesRouter);
+server.use("/api/items", itemsRouter);
 
-server.use((err, req, res, next) => {
-    console.log(err)
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+// server.use((err, req, res, next) => {
+//     console.log(err)
 
-    res.status(500).json({
-        message: "Something went wrong",
-    })
-})
+//     res.status(500).json({
+//         message: "Something went wrong",
+//     })
+// })
 
-server.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`)
-})
+server.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
