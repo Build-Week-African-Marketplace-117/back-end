9724179fe5afabd300c9a6dd7bfa88c6d74849c6
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-02-04 18:50:33 -0600

    Trying changed file to see if it helps deployment

--- data/config.js
+++ data/config.js
@@ -1,5 +1,4 @@
 const knex = require("knex");
-const knexfile = require("../knexfile")
 const env = process.env.DB_ENV || "development";
 const config = require("../knexfile");
 const db = knex(config[env]);
